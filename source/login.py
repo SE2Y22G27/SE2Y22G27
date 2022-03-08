71e9ddc29a2c99d307f26fef1ce5316d22ad5b6a
Exelics <modsmanr4y@gmail.com>
2022-03-08 13:41:58 +0700

    Changed import in register and added login functionality

---
+++ source/login.py
@@ -0,0 +1,52 @@
+from source.database import data
+from source.helpers_auth import generate_session_id, generate_token
+import hashlib
+
+def login(email: str, password: str):
+	'''
+	Given an email and password, logs the user into the service.
+	
+	Arguments:
+		email (string) - Email of the user
+		password (string) - Password of the user
+
+	Exceptions:
+		- Email does not exist in the database
+		- Password is incorrect
+
+	Returns:
+		The authorized user id and token of the user.
+	'''
+	database = data.get_data()
+	u_id = 0
+	current_token = 0
+	email_exist = False
+	correct_password = False
+
+	# Check if the email exists firsts,
+	# if email exists then check password next
+	# This should create a session ID and token
+	# for the user to login.
+	for user in database['users']:
+		if user['email'] == email:
+			email_exist = True
+			check_password = hashlib.sha256(password.encode()).hexdigest()
+			if check_password == user['password']:
+				correct_password = True
+				temp_session = generate_session_id()
+				u_id = user['user_id']
+				current_token = generate_token(temp_session, u_id)
+				user['sessions'][current_token] = temp_session
+
+	# ----- Error handling -----
+	if email_exist is False:
+		raise Exception("This email does not exist")
+	elif correct_password is False:
+		raise Exception("Incorrect password")
+	# ----- End of Error handling -----
+	
+	data.save_data(database)
+	return {
+		'auth_user_id': u_id,
+		'token': current_token,
+	}

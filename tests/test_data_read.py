44473a97196725c80caa6593b901ac90243261f8
LC731816289 <100256970+LC731816289@users.noreply.github.com>
2022-03-10 19:55:29 +1100

    added one extra key invoiceID and included persistance by storing data into pickle file

--- tests/test_data_read.py
+++ tests/test_data_read.py
@@ -1,14 +1,17 @@
 from calendar import c
 import pytest
 from source.data_read import data_read_v1
+from source.database import data
 from source.register import register
 from source.test_clear import clear
+from source.error import InputError
 
 def test_valid_input():
     clear()
     id = register("test0@gmail.com", "password", "I", "Person")
 
-    sample_dict = { 'Amount' : 10, 
+    sample_dict = { 'invoiceID' : 1,
+                    'Amount' : 10, 
                     'lineExtensionAmount' : 6,
                     'taxExclusiveAmount' : 4,
                     'taxInclusiveAmount' : 9,
@@ -17,7 +20,36 @@ def test_valid_input():
                     'items' : [{'invoiceQuantity' : 5, 'lineExtensionAmount' : 15, 'priceAmount' : 26.61},],
                     }
 
-    with pytest.raises(Exception):
+    data_read_v1(id['token'], sample_dict)         
+    user_invoices = {}
+    test_dict = {}
+    data.load_data()
+    data_dict = data.get_data()
+    for user in data_dict['users']:
+        if user['user_id'] == id['auth_user_id']:
+            user_invoices = user['user_invoices']
+            break
+
+    for invoice in user_invoices:
+        if invoice['invoiceID'] == 1:
+            test_dict = invoice
+            break
+
+    assert  test_dict == sample_dict
+
+def test_invalid_taxInclusiveAmount():
+    clear()
+    id = register("test0@gmail.com", "password", "I", "Person")
+
+    sample_dict = { 'Amount' : 10, 
+                    'lineExtensionAmount' : 6,
+                    'taxExclusiveAmount' : 4,
+                    'taxInclusiveAmount' : {},
+                    'chargeTotalAmount' : 332.6,
+                    'payableAmount' : 332.6,
+                    'items' : [{'invoiceQuantity' : 5, 'lineExtensionAmount' : 15, 'priceAmount' : 26.61},],
+                    }
+    with pytest.raises(InputError):
         data_read_v1(id['token'], sample_dict)
 
 def test_invalid_taxInclusiveAmount():
@@ -33,22 +65,7 @@ def test_invalid_taxInclusiveAmount():
                     'items' : [{'invoiceQuantity' : 5, 'lineExtensionAmount' : 15, 'priceAmount' : 26.61},],
                     }
 
-    assert data_read_v1(id['token'], sample_dict)
-
-def test_invalid_taxInclusiveAmount():
-    clear()
-    id = register("test0@gmail.com", "password", "I", "Person")
-
-    sample_dict = { 'Amount' : 10, 
-                    'lineExtensionAmount' : 6,
-                    'taxExclusiveAmount' : 4,
-                    'taxInclusiveAmount' : {},
-                    'chargeTotalAmount' : 332.6,
-                    'payableAmount' : 332.6,
-                    'items' : [{'invoiceQuantity' : 5, 'lineExtensionAmount' : 15, 'priceAmount' : 26.61},],
-                    }
-
-    with pytest.raises(Exception):
+    with pytest.raises(InputError):
         data_read_v1(id['token'], sample_dict)
 
 def test_invalid_items():
@@ -64,7 +81,7 @@ def test_invalid_items():
                     'items' : [{}],
                     }
 
-    with pytest.raises(Exception):
+    with pytest.raises(InputError):
         data_read_v1(id['token'], sample_dict)
 
 def test_invalid_item_information():
@@ -80,5 +97,5 @@ def test_invalid_item_information():
                     'items' : [{'invoiceQuantity' : {}, 'lineExtensionAmount' : 15, 'priceAmount' : 26.61},],
                     }
 
-    with pytest.raises(Exception):
+    with pytest.raises(InputError):
         data_read_v1(id['token'], sample_dict)

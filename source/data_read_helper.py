397a6fb7c5a16d9602d76b982f67f4dd5efc6d83
LC731816289 <100256970+LC731816289@users.noreply.github.com>
2022-03-11 13:41:51 +1100

    added more helper function to test out each field if they contains valid data and is enough to generate an invoice

--- source/data_read_helper.py
+++ source/data_read_helper.py
@@ -1,3 +1,5 @@
+
+
 def check_valid_data(data):
     '''
         checks whether the keys are all assigned with a value if not then the user
@@ -11,29 +13,90 @@ def check_valid_data(data):
                    by the user is sufficent to generate an invoice
 
     '''
-    # check if the items are not empty
-    for key, value in data.items():
-        if value == {}:
-            return False
-    
-    return check_valid_items(data['items'])
+    # check if the invoiceTypeCode are correct
+    if not check_valid_InvoiceTypeCode(data['InvoiceTypeCode']):
+        return False
 
-def check_valid_items(data_items):
-    '''
-        checks whether the items have correct quantity, amount, and total price amount
+    # check if the allowance are correct
+    if not check_valid_AllowanceCharge(data['AllowanceCharge']):
+        return False
 
-        Arguments:
-            data (dictionary) - information contains items key
+    # check if the legalmonetary total are correct
+    if not check_valid_LegalMonetaryTotal(data['LegalMonetaryTotal']):
+        return False
 
-        Returns:
-            bool - if all the required fields are not empty dictionary then the items are valid
-
-    '''
-    # check if the item is valid by checking if all the keys are not {}
-    for item in data_items:
-        for key,value in item.items():
-            if value == {}:
-                return False
+    # check if all the invoiceLine are correct
+    if not check_valid_InvoiceLine(data['InvoiceLine']):
+        return False
 
     return True
 
+def check_valid_InvoiceTypeCode(data_invoiceTypeCode):
+
+    if data_invoiceTypeCode == {}:
+        return False
+    
+    if data_invoiceTypeCode != 380:
+        return False
+
+    return True
+
+
+def check_valid_AllowanceCharge(data_AllowanceCharge):
+
+    for  value in data_AllowanceCharge.values():
+        if value == {}:
+            return False
+
+    for value in data_AllowanceCharge['TaxCatagory'].values():
+        if value == {}:
+            return False
+    
+    if data_AllowanceCharge['TaxCatagory']['ID'] == {}:
+        return False
+
+    check_valid_ChargeAmount(data_AllowanceCharge['Amount'])
+
+    return True
+
+    
+        
+
+def check_valid_LegalMonetaryTotal(data_LegalMonetaryTotal):
+
+    for value in data_LegalMonetaryTotal.values():
+        if value == {}:
+            return False
+    check_valid_ChargeAmount(data_LegalMonetaryTotal['LineExtensionAmount'])
+    check_valid_ChargeAmount(data_LegalMonetaryTotal['TaxInclusiveAmount'])
+    check_valid_ChargeAmount(data_LegalMonetaryTotal['TaxExclusiveAmount'])
+    check_valid_ChargeAmount(data_LegalMonetaryTotal['ChargedTotalAmount'])
+    check_valid_ChargeAmount(data_LegalMonetaryTotal['PayableAmount'])
+
+    return True
+
+def check_valid_InvoiceLine(data_InvoiceLine):
+
+    for  invoice_item in data_InvoiceLine:
+        for value in invoice_item.values():
+            if value == {}:
+                return False
+            
+        if invoice_item['Price']['PriceAmount'] == {}:
+            return False
+        
+        check_valid_PriceAmount(invoice_item['Price']['PriceAmount'])
+
+    return True
+
+def check_valid_ChargeAmount(data_amount):
+    if data_amount < 0:
+        return True
+
+    return False
+
+def check_valid_PriceAmount(data_amount):
+    if data_amount >= 0:
+        return True
+
+    return False

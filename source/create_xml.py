77cf14219a9bd4f2875cae9d6b4379fcb1d8c523
Elliot Berghofer <z5346037@ad.unsw.edu.au>
2022-03-11 13:06:28 +1100

    Updated the xml tree data based on the new confluence database structure

--- source/create_xml.py
+++ source/create_xml.py
@@ -39,13 +39,47 @@ def create_invoice_v1(token):
 
     root = ET.Element("Invoice")
 
+    # Section 1. Invoice Type Code
+    ET.SubElement(root, f"{cbc}InvoiceTypeCode").text = f"{invoice_dict["InvoiceTypeCode"]}"
+
+    # Section 2. Allowance Charge
+    allowance_charge = ET.SubElement(root, f"{cac}AllowanceCharge")
+    allowance_charge_dict = invoice_dict["AllowanceCharge"]
+    ET.SubElement(allowance_charge, f"{cbc}ChargeIndicator").text = f"{allowance_charge_dict["ChargeIndicator"]}"
+    ET.SubElement(allowance_charge, f"{cbc}AllowanceChargeReason").text = f"{allowance_charge_dict["AllowanceChargeReason"]}"
+    ET.SubElement(allowance_charge, f"{cbc}Amount", currencyID=f"{cID}").text = f"{allowance_charge_dict["Amount"]}"
+
+    tax_category = ET.SubElement(allowance_charge, f"{cac}TaxCategory")
+    tax_category_dict = invoice_dict["tax_catagory"]
+    ET.SubElement(tax_category, f"{cbc}ID").text = f"{tax_category_dict["ID"]}"
+    ET.SubElement(tax_category, f"{cbc}Percent").text =  f"{tax_category_dict["Percent"]}"
+
+    tax_scheme = ET.SubElement(tax_category, f"{cac}TaxScheme")
+    tax_scheme_dict = invoice_dict["TaxScheme"]
+    ET.SubElement(tax_scheme, f"{cbc}ID").text = f"{tax_scheme_dict["ID"]}"
+
     # Section 3. LegalMonetaryTotal
     legal_monetary_total = ET.SubElement(root, f"{cac}LegalMonetaryTotal")
-    ET.SubElement(legal_monetary_total, f"{cbc}LineExtensionAmount", currencyID=f"{cID}").text = f"{invoice_dict["lineExtensionAmount"]}"
-    ET.SubElement(legal_monetary_total, f"{cbc}TaxExclusiveAmount", currencyID=f"{cID}").text = f"{invoice_dict["taxExclusiveAmount"]}"
-    ET.SubElement(legal_monetary_total, f"{cbc}TaxInclusiveAmount", currencyID=f"{cID}").text = f"{invoice_dict["taxInclusiveAmount"]}"
-    ET.SubElement(legal_monetary_total, f"{cbc}ChargeTotalAmount", currencyID=f"{cID}").text = f"{invoice_dict["chargeTotalAmount"]}"
-    ET.SubElement(legal_monetary_total, f"{cbc}PayableAmount", currencyID=f"{cID}").text = f"{invoice_dict["payableAmount"]}"
+    legal_monetary_total_dict = invoice_dict["LegalMonetaryTotal"]
+    ET.SubElement(legal_monetary_total, f"{cbc}LineExtensionAmount", currencyID=f"{cID}").text = f"{legal_monetary_total_dict["lineExtensionAmount"]}"
+    ET.SubElement(legal_monetary_total, f"{cbc}TaxExclusiveAmount", currencyID=f"{cID}").text = f"{legal_monetary_total_dict["taxExclusiveAmount"]}"
+    ET.SubElement(legal_monetary_total, f"{cbc}TaxInclusiveAmount", currencyID=f"{cID}").text = f"{legal_monetary_total_dict["taxInclusiveAmount"]}"
+    ET.SubElement(legal_monetary_total, f"{cbc}ChargeTotalAmount", currencyID=f"{cID}").text = f"{legal_monetary_total_dict["chargeTotalAmount"]}"
+    ET.SubElement(legal_monetary_total, f"{cbc}PayableAmount", currencyID=f"{cID}").text = f"{legal_monetary_total_dict["payableAmount"]}"
+
+
+    # Section 4 / 5. InvoiceLines.
+    # Just repeates it twice for completeness.
+    invoice_line_list = invoice_dict["InvoiceLine"]
+    for invoice_line_dict in invoice_line_list:
+        invoice_line = ET.SubElement(root, f"{cac}InvoiceLine")
+        ET.SubElement(invoice_line, f"{cbc}ID").text = f"{invoice_line_dict["ID"]}"
+        ET.SubElement(invoice_line, f"{cbc}InvoicedQuantity", unitCode="DAY").text = f"{invoice_line_dict["InvoicedQuantity"]}"
+        ET.SubElement(invoice_line, f"{cbc}LineExtensionAmount", currencyID=f"{cID}").text =  f"{invoice_line_dict["LineExtensionAmount"]}"
+
+        price = ET.SubElement(invoice_line, f"{cac}Price")
+        ET.SubElement(price, f"{cbc}PriceAmount", currencyID=f"{cID}").text = f"{invoice_line_dict["PriceAmount"]}"
+
 
     tree = ET.ElementTree(root)
 

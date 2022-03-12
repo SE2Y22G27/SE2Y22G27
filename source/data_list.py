4eab87dbe5a8ab32ff64c676a59210cf9b93b348
LC731816289 <100256970+LC731816289@users.noreply.github.com>
2022-03-12 14:09:16 +1100

    fixed the code and added AccessError

--- source/data_list.py
+++ source/data_list.py
@@ -1,6 +1,7 @@
 from source.database import data
 from source.helpers_auth import check_valid_token, decode_token
 from source.data_read_helper import decode_user_invoice
+from source.error import AccessError
 import json
 
 def data_list_v1(token):
@@ -11,18 +12,20 @@ def data_list_v1(token):
             token(String) : jwt token that stores user id
 
         Returns:
-            void
+            return the dictionary of invoice information
     '''
 
     check_valid_token(token)
 
     user_id = decode_token(token)
 
-    for user in data['users']:
+    data_dict = data.get_data()
+
+    for user in data_dict['users']:
         if user['user_id'] == user_id:
             if user['user_invoice'] == {}:
-                print("you have saved any invoice in your account")
+                raise AccessError("you have saved any invoice in your account")
             else:
                 invoice_dict = decode_user_invoice(user['user_invoice'])
-                print(invoice_dict)
+                return invoice_dict
             break

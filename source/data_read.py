1cccde320d281b6671c9d60afc3e56c509d41243
LC731816289 <100256970+LC731816289@users.noreply.github.com>
2022-03-12 13:34:01 +1100

    finish encode all data

--- source/data_read.py
+++ source/data_read.py
@@ -1,4 +1,6 @@
 import json
+import jwt
+from source.config import secret
 from source.database import data as database
 from source.helpers_auth import check_valid_token, decode_token
 from source.data_read_helper import check_valid_data
@@ -8,15 +10,12 @@ def data_read_v1(token, data):
     '''
     given token (username) and session_id to check whether this user is currently logged into our system
     data is the json_input from the user to be stored under their users dictionary
-
     Arguments:
         token (string) : Username thats encrypted into JWT token
         data (string) : json data received from the user input
-
     Exceptions:
         - invalid token
         - data is not valid 
-
     Returns:
         {}
     
@@ -32,23 +31,36 @@ def data_read_v1(token, data):
         raise InputError("no sufficient information to create an invoice")
 
     user_id = decode_token(token)
+    
+    invoice_dict =  {
+                    'InvoiceTypeCode' : data['InvoiceTypeCode'],
 
-    invoice_dict =  {'invoiceID' : data['invoiceID'],
-                    'Amount' : data['Amount'], 
-                    'lineExtensionAmount' : data['lineExtensionAmount'],
-                    'taxExclusiveAmount' : data['taxExclusiveAmount'],
-                    'taxInclusiveAmount' : data['taxInclusiveAmount'],
-                    'chargeTotalAmount' : data['chargeTotalAmount'],
-                    'payableAmount' : data['payableAmount'],
-                    'items' : [],
+                    'AllowanceCharge' :     {
+                                                'ChargeIndicator' : data['AllowanceCharge']['ChargeIndicator'],
+                                                'AllowanceChargeReason' : data['AllowanceCharge']['AllowanceChargeReason'],
+                                                'Amount' : data['AllowanceCharge']['Amount'],
+                                                'TaxCatagory' : {   'ID' : data['AllowanceCharge']['TaxCatagory']['ID'],
+                                                                    'Percent' : data['AllowanceCharge']['TaxCatagory']['Percent'],
+                                                                    'TaxScheme' : { 'ID' : data['AllowanceCharge']['TaxCatagory']['TaxScheme']['ID']},
+                                                                },
+                                            }, 
+
+                    'LegalMonetaryTotal' : {    'LineExtensionAmount' : data['LegalMonetaryTotal']['LineExtensionAmount'],
+                                                'TaxExclusiveAmount' : data['LegalMonetaryTotal']['TaxExclusiveAmount'],
+                                                'TaxInclusiveAmount' : data['LegalMonetaryTotal']['TaxInclusiveAmount'],
+                                                'ChargedTotalAmount' : data['LegalMonetaryTotal']['ChargedTotalAmount'],
+                                                'PayableAmount' : data['LegalMonetaryTotal']['PayableAmount'],
+                                            },
+
+                    'InvoiceLine' : [],
                     }
-
-    for items in data['items']:
-        invoice_dict['items'].append(items)
+    
+    for items in data['InvoiceLine']:
+        invoice_dict['InvoiceLine'].append(items)
 
     for user in data_info['users']:
         if user["user_id"] == user_id:
-            user['user_invoices'].append(invoice_dict)
+            user['user_invoice'] = jwt.encode(invoice_dict, secret, algorithm= 'HS256')
             break
 
     database.save_data(data_info)

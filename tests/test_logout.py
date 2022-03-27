8daaf209681a3e0741eb437d17014d15c4c8c16d
LC731816289 <100256970+LC731816289@users.noreply.github.com>
2022-03-28 10:18:31 +1100

    fixed pylint error

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -1,32 +1,41 @@
+'''
+	imported all the required module to test of authorised user logout
+	successfully
+'''
 import pytest
 from source.error import AccessError
 from source.register import register
-from source.login import login
 from source.logout import logout
 from source.test_clear import clear
 
-@pytest.fixture
-def initial_clear():
-	clear()
+def test_invalid_token_format():
+    '''
+        test when entering invalid token format to logout
+    '''
+    clear()
+    register("testA@gmail.com", "1234567890", "Person", "AA")
+    test_token = "hello"
+    with pytest.raises(AccessError):
+        logout(test_token)
 
-@pytest.fixture
-def register_a():
-	register_info = register("testA@gmail.com", "1234567890", "Person", "AA")
-	return register_info
+def test_invalid_token():
+    '''
+        test when entering invalid token to logout
+	'''
+    clear()
+    register("testA@gmail.com", "1234567890", "Person", "AA")
+    test_token = "aaa.bbb.ccc"
+    with pytest.raises(AccessError):
+        logout(test_token)
 
-def test_invalid_token_format(initial_clear, register_a):
-	test_token = "hello"
-	with pytest.raises(AccessError):
-		logout(test_token)
-
-def test_invalid_token(initial_clear, register_a):
-	test_token = "aaa.bbb.ccc"
-	with pytest.raises(AccessError):
-		logout(test_token)
-
-def test_logout_success(initial_clear, register_a):
+def test_logout_success():
+    '''
+		test when entering valid token to logout
+	'''
 	# Nothing is supposed to happen here since logout
 	# does not return anything. So expected behaviour is that
 	# this test passes without any errors.
-	token = register_a['token']
-	logout(token)
+    clear()
+    register_a = register("testA@gmail.com", "1234567890", "Person", "AA")
+    token = register_a['token']
+    logout(token)

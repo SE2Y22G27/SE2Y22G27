5b72f232cbf799e5f48e0ae0143c0b4ab45d3f39
LC731816289 <100256970+LC731816289@users.noreply.github.com>
2022-03-28 09:33:21 +1100

    fixed pylint error

--- tests/test_data_list.py
+++ tests/test_data_list.py
@@ -1,66 +1,70 @@
+'''
+    import pytest to test if the source code behave what we've expected
+'''
 from source.data_list import data_list_v1
-from source.database import data
 from source.test_clear import clear
-from source.data_read_helper import decode_user_invoice
 from source.register import register
 from source.error import AccessError
-import pytest
 from source.data_read import data_read_v1
+import pytest
 
 def test_valid_invoice():
+    '''
+        testing if the invoice is listing porperly
+    '''
     clear()
-    id = register("test0@gmail.com", "password", "I", "Person")
+    register_info = register("testA@gmail.com", "1234567890", "Person", "AA")
 
     sample_dict = { 'InvoiceTypeCode' : 380,
+        'AllowanceCharge' : {
+            'ChargeIndicator' : 'true',
+            'AllowanceChargeReason' : 'Insurance',
+            'Amount' : -25,
+            'TaxCategory' : {   'ID' : 'S',
+                'Percent' : 25.0,
+                'TaxScheme' : { 'ID' : 'VAT'},
+                            },
+                    },
 
-                    'AllowanceCharge' :     {
-                                                'ChargeIndicator' : 'true',
-                                                'AllowanceChargeReason' : 'Insurance',
-                                                'Amount' : -25,
-                                                'TaxCategory' : {   'ID' : 'S',
-                                                                    'Percent' : 25.0,
-                                                                    'TaxScheme' : { 'ID' : 'VAT'},
-                                                                },
-                                            }, 
+        'LegalMonetaryTotal' : {'LineExtensionAmount' : -1300,
+                                'TaxExclusiveAmount' : -1000,
+                                'TaxInclusiveAmount' : -1656.25,
+                                'ChargedTotalAmount' : -25,
+                                'PayableAmount' : -1656.25,
+                                },
 
-                    'LegalMonetaryTotal' : {    'LineExtensionAmount' : -1300,
-                                                'TaxExclusiveAmount' : -1000,
-                                                'TaxInclusiveAmount' : -1656.25,
-                                                'ChargedTotalAmount' : -25,
-                                                'PayableAmount' : -1656.25,
-                                            },
-
-                    'InvoiceLine' : [
-                                        {
-                                            'ID' : 1, 
-                                            'InvoiceQuantity' : -7,
-                                            'LineExtensionAmount' : -2800,
-                                            'Price' : {
-                                                        'PriceAmount' : 400,
-                                                      },
-                                        },
-                                        {
-                                            'ID' : 2, 
-                                            'InvoiceQuantity' : 3,
-                                            'LineExtensionAmount' : 1500,
-                                            'Price' : {
-                                                        'PriceAmount' : 500,
-                                                      },
-                                        },
-                                    ],
-                    
+        'InvoiceLine' : [
+            {
+                'ID' : 1,
+                'InvoiceQuantity' : -7,
+                'LineExtensionAmount' : -2800,
+                'Price' : {
+                    'PriceAmount' : 400,
+                        },
+            },
+            {
+                'ID' : 2,
+                'InvoiceQuantity' : 3,
+                'LineExtensionAmount' : 1500,
+                'Price' : {
+                    'PriceAmount' : 500,
+                            },
+            },
+                        ],
                     }
 
-    data_read_v1(id['token'], sample_dict)   
-    test_dict = data_list_v1(id['token'])
+    data_read_v1(register_info['token'], sample_dict)
+    test_dict = data_list_v1(register_info['token'])
 
     assert  test_dict == sample_dict
 
 def test_empty_invoice():
+    '''
+        testing when a user haven't enter their input
+    '''
     clear()
-    id = register("test0@gmail.com", "password", "I", "Person")
+    register_info = register("test0@gmail.com", "password", "I", "Person")
 
-    
     with pytest.raises(AccessError):
-        data_list_v1(id['token'])
+        data_list_v1(register_info['token'])
    

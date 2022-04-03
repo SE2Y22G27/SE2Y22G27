3315911472fd69012696f7e27fbbd018477b80a5
Elliot Berghofer <z5346037@ad.unsw.edu.au>
2022-04-03 14:27:35 +1000

    Adding functionality to display a pdf

--- app_frontend.py
+++ app_frontend.py
@@ -14,7 +14,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template('index.html')
+    return render_template('display_invoice.html')
+    #return render_template('index.html')
 
 @app.route("/register")
 def register_page():

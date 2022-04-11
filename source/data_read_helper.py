dead0225229ccce5c993b07d20b8c83e347fd14d
LC731816289 <100256970+LC731816289@users.noreply.github.com>
2022-04-11 17:48:49 +1000

    added new helper and modified the old one to satisfy the new conditions for new fields

--- source/data_read_helper.py
+++ source/data_read_helper.py
@@ -4,6 +4,8 @@
 '''
 import jwt
 from source.config import secret
+from datetime import datetime
+import time
 def check_valid_data(data):
     '''
         checks whether the keys are all assigned with a value if not then the user
@@ -17,12 +19,16 @@ def check_valid_data(data):
                    by the user is sufficent to generate an invoice
 
     '''
+    for info in data.values():
+        if info == {}:
+            return False
+
     # check if the invoiceTypeCode are correct
     if not check_valid_typecode(data['InvoiceTypeCode']):
         return False
 
     # check if the allowance are correct
-    if not check_valid_allowancecharge(data['AllowanceCharge']):
+    if not check_valid_taxtotal(data['TaxTotal']):
         return False
 
     # check if the legalmonetary total are correct
@@ -31,6 +37,15 @@ def check_valid_data(data):
 
     # check if all the invoiceLine are correct
     if not check_valid_invoiceline(data['InvoiceLine']):
+        return False
+
+    if not check_valid_issuedate(data['IssueDate']):
+        return False
+
+    if not check_valid_supplier(data['AccountingSupplierParty']['Party']):
+        return False
+
+    if not check_valid_customer(data['AccountingCustomerParty']['Party']):
         return False
 
     return True
@@ -48,22 +63,61 @@ def check_valid_typecode(data_invoicetypecode):
 
     return True
 
-def check_valid_allowancecharge(data_allowancecharge):
+def check_valid_issuedate(data_issuedate):
+    '''
+        check if the issue date is valid
+    '''
+    if data_issuedate == {}:
+        return False
+
+    if data_issuedate > datetime.now().strftime("%Y-%m-%d"):
+        return False
+
+    return True
+
+def check_valid_paymentmean(data_paymentmean):
+    '''
+        check if the payment mean is valid
+    '''
+    if data_paymentmean == {}:
+        return False
+
+    for info in data_paymentmean:
+        if info.values() == {}:
+            return False
+    return True
+
+def check_valid_payment_term(data_payment_term):
+    '''
+        check if the payment terms is valid
+    '''
+    if data_payment_term == {}:
+        return False
+
+    if data_payment_term['Note'] == {}:
+        return False
+    
+    return True
+
+
+def check_valid_taxtotal(data_taxtotal):
     '''
         check if all the allowance chage fields are not empty
     '''
-    for  value in data_allowancecharge.values():
+    for  value in data_taxtotal.values():
         if value == {}:
             return False
 
-    for value in data_allowancecharge['TaxCategory'].values():
+    for value in data_taxtotal['TaxSubtotal'].values():
         if value == {}:
             return False
 
-    if data_allowancecharge['TaxCategory']['ID'] == {}:
-        return False
+    for value in data_taxtotal['TaxSubtotal']['TaxCategory'].values():
+        if value == {}:
+            return False
 
-    check_valid_chargeamount(data_allowancecharge['Amount'])
+    if  data_taxtotal['TaxSubtotal']['TaxCategory']['TaxScheme']['ID'] == {}:
+            return False
 
     return True
 
@@ -78,7 +132,7 @@ def check_valid_legalmonetarytotal(data_legalmonetarytotal):
     check_valid_chargeamount(data_legalmonetarytotal['LineExtensionAmount'])
     check_valid_chargeamount(data_legalmonetarytotal['TaxInclusiveAmount'])
     check_valid_chargeamount(data_legalmonetarytotal['TaxExclusiveAmount'])
-    check_valid_chargeamount(data_legalmonetarytotal['ChargedTotalAmount'])
+    check_valid_chargeamount(data_legalmonetarytotal['PayableRoundingAmount'])
     check_valid_chargeamount(data_legalmonetarytotal['PayableAmount'])
 
     return True
@@ -92,10 +146,74 @@ def check_valid_invoiceline(data_invoiceline):
             if value == {}:
                 return False
 
+        for value in invoice_item['Item'].values():
+
+            if value == {}:
+                return False
+
+        for value in invoice_item['Item']['ClassifiedTaxCategory'].values():
+
+            if value == {}:
+                return False
+
+        if  invoice_item['Item']['ClassifiedTaxCategory']['TaxScheme']['ID'] == {}:
+            return False
+
         if invoice_item['Price']['PriceAmount'] == {}:
+            return False
+            
+        if invoice_item['Price']['BaseQuantity'] == {}:
             return False
 
         check_valid_priceamount(invoice_item['Price']['PriceAmount'])
+        check_valid_priceamount(invoice_item['Price']['BaseQuantity'])
+
+    return True
+
+def check_valid_supplier(data_supplier):
+    '''
+        check if all the fields are not empty
+    '''
+    for info in data_supplier:
+        if info == {}:
+            return False
+
+    
+    if  data_supplier['PartyIdentification']['ID'] == {}:
+            return False
+
+    if  data_supplier['PartyName']['Name'] == {}:
+            return False
+    for supplier_info in data_supplier['PostalAddress']:
+            if  supplier_info == {}:
+                return False
+    if data_supplier['PostalAddress']['Country']['IdentificationCode'] == {}:
+            return False
+    if  data_supplier['PartyLegalEntity']['RegistrationName'] == {}:
+            return False
+    if  data_supplier['PartyLegalEntity']['CompanyID'] == {}:
+            return False
+
+    return True
+
+def check_valid_customer(data_customer):
+    '''
+        check if all the fields are not empty
+    '''
+    for info in data_customer:
+        if info == {}:
+            return False
+
+
+    if  data_customer['PartyName']['Name'] == {}:
+            return False
+    for supplier_info in data_customer['PostalAddress']:
+            if  supplier_info == {}:
+                return False
+    if data_customer['PostalAddress']['Country']['IdentificationCode'] == {}:
+            return False
+    if  data_customer['PartyLegalEntity']['RegistrationName'] == {}:
+            return False
 
     return True
 
@@ -103,7 +221,7 @@ def check_valid_chargeamount(data_amount):
     '''
         check if charge amount satisfies the condition
     '''
-    if int(data_amount) < 0:
+    if int(data_amount) >= 0:
         return True
 
     return False

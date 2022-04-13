d557e7befdfb49a0b8ae2dc0ac81cf7c71004ad6
LC731816289 <100256970+LC731816289@users.noreply.github.com>
2022-04-13 20:19:02 +1000

    finish connecting to other teams validation and rendering api

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from importlib.metadata import files
 import sys
+import requests
 from json import dumps, loads
 from flask import Flask, request, Response, render_template
 from source.data_read import data_read_v1
@@ -58,41 +60,143 @@ def create_xml_route():
     #info = request.get_json()
     token = request.form['JWTToken']
     
-    invoice_dict = { 'InvoiceTypeCode' : 380,
-        'AllowanceCharge' : {
-            'ChargeIndicator' : request.form['ChargeIndicator'],
-            'AllowanceChargeReason' : request.form['AllowanceChargeReason'],
-            'Amount' : request.form['Amount'],
-            'TaxCategory' : {   'ID' : request.form['TaxCategoryID'],
-                'Percent' : request.form['Percent'],
-                'TaxScheme' : { 'ID' : request.form['TaxSchemeID']},
-                            },
+    
+
+
+    invoice_dict = { 
+        'InvoiceTypeCode' : 380,
+        'InvoiceID' : request.form['InvoiceID'],
+        'IssueDate' : request.form['IssueDate'],
+        'AccountingSupplierParty' : {
+            'Party' : 
+                    {
+                        'PartyIdentification': {'ID':  request.form['RegistrationID']},
+                        'PartyName' : {'Name':  request.form['Name']},
+                        'PostalAddress' : {
+                                'StreetName' :  request.form['Street'],
+                                'CityName':  request.form['City'],
+                                'PostalZone' :  request.form['PostalZone'],
+                                'Country' : {'IdentificationCode': 'AU'}
+                        },
+                        'PartyLegalEntity' : {
+                            'RegistrationName' :  request.form['Name'],
+                            'CompanyID' :  request.form['RegistrationID'],
+                        }
+                    }
+
+                                     },
+        'AccountingCustomerParty' : {
+            'Party' : 
+                    {
+                        'PartyName' : {'Name':  request.form['Name1']},
+                        'PostalAddress' : {
+                                'StreetName' :  request.form['Street1'],
+                                'CityName':  request.form['CityName'],
+                                'PostalZone' : request.form['PostalZone'],
+                                'Country' : {'IdentificationCode': 'AU'}
+                        },
+                        'PartyLegalEntity' : {
+                            'RegistrationName' :  request.form['Name1'],
+                        }
+                    }
+
+                                     },
+        'PaymentMeans': {
+            'PaymentMeansCode' : 1,
+            'PaymentID' :  request.form['InvoiceID'],
+                        },
+        'PaymentTerms': {
+            'Note' : 'As agreed'
+                        },
+
+        'TaxTotal' : {
+            'TaxAmount' :  request.form['TaxAmount'],
+            'TaxSubtotal' : {
+                'TaxableAmount' :  request.form['TaxableAmount'],
+                'TaxAmount' :  request.form['SubTaxAmount'],
+                'TaxCategory' : {  
+                     'ID' :  request.form['TaxCategoryID'],
+                'Percent' :  request.form['TaxCategoryPercent'],
+                'TaxScheme' : { 
+                    'ID' :  request.form['TaxSchemeID']
+                             }
+                                },
                     },
 
-        'LegalMonetaryTotal' : {'LineExtensionAmount' : request.form['LineExtensionAmount'],
+                    },
+            
+
+        'LegalMonetaryTotal' : {'LineExtensionAmount' :  request.form['LineExtensionAmount'],
                                 'TaxExclusiveAmount' : request.form['TaxExclusiveAmount'],
                                 'TaxInclusiveAmount' : request.form['TaxInclusiveAmount'],
-                                'ChargedTotalAmount' : request.form['ChargedTotalAmount'],
+                                'PayableRoundingAmount' : request.form['PayableRoundingAmount'],
                                 'PayableAmount' : request.form['PayableAmount'],
                                 },
 
-        'InvoiceLine' : [
-            {
-                'ID' : request.form['InvoiceLineID'],
-                'InvoiceQuantity' : request.form['InvoicedQuantity'],
-                'LineExtensionAmount' : request.form['LineExtensionAmount'],
-                'Price' : {
-                    'PriceAmount' : request.form['PriceAmount'],
-                        }
-            }
-                        ],
+        'InvoiceLine' : [],
                     }
     
-    
+    if 'AdditionalStreet' in request.form:
+        invoice_dict['AccountingSupplierParty']['Party']['PostalAddress']['AdditionalStreetName'] = request.form["AdditionalStreet"]
+    if 'AdditionalStreet1' in request.form:
+        invoice_dict['AccountingCustomerParty']['Party']['PostalAddress']['AdditionalStreetName'] = request.form["AdditionalStreet1"]
+
+    digit = 0
+
+    while 'ID'+f"{digit}" in request.form:
+        invoice_line_dict = {
+                'ID' :  request.form['ID'+f"{digit}"],
+                'InvoiceQuantity' : request.form['InvoiceQuantity'+f"{digit}"],
+                'LineExtensionAmount' : request.form['LineExtensionAmount'+f"{digit}"],
+                'Item' : {
+                    'Name' : request.form['ItemName'+f"{digit}"],
+                    'ClassifiedTaxCategory': {
+                        'ID':request.form['ClassifiedTaxCategoryID'+f"{digit}"],
+                        'Percent': request.form['ClassifiedTaxCategoryPercent'+f"{digit}"],
+                        'TaxScheme': {'ID' :request.form['ClassifiedTaxCategoryTaxSchemeID'+f"{digit}"]},
+                    },
+                },
+                'Price' : {
+                    'PriceAmount' : request.form['PriceAmount'+f"{digit}"],
+                    'BaseQuantity' : request.form['BaseQuantity'+f"{digit}"],
+                        },
+            }
+        invoice_dict['InvoiceLine'].append(invoice_line_dict)
+        digit = digit + 1
+
     data_read_v1(token, invoice_dict)
     create_invoice_v1(token)
     
+    validation_endpoint = "http://invoicevalidation.services:8080/verify_invoice"
+    user_id = decode_token(token)
+    files = {'invoice': open(f"{user_id}"+"_invoice.xml",'rb')}
+    data_dict = {
+        'token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJncm91cF9pZCI6NX0.GoDgfC3GzSjjOgKhntzyd37euX0ec-v5G4P-rKG7V3A',
+        'rules' : 3,
+        'output_type' : 'json'
+    }
+    response = requests.post(url = validation_endpoint, files= files, data = data_dict)
+    data = response.json()
+    
+    if response.status_code != 200:
+        print("server is out!!!")
+    if data['message']['valid'] == True:
+        rendering_endpoint = "https://www.invoicerendering.com/einvoices/v2"
+        param = {
+            'renderType' : "html",
+            'lang' : "en"
+        }
+        files = {
+            'xml' : open(f"{user_id}"+"_invoice.xml","rb")
+        }
+        response = requests.post(url= rendering_endpoint, params= param, files= files)
+        if response.status_code == 200:
+            f = open(f"{user_id}"+"render.html", "w")
+            f.write(response.text)
+        print(response.text)
+
     return render_template('display_invoice.html', token=token)
+    
 
 @app.route("/invoice/send/v1", methods = ['POST'])
 def send_invoice():
